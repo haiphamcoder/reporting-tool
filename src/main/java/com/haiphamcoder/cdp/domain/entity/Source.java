6dc858a646ea851993e3f93db9a182d33a681141
Hai Pham Ngoc <ngochai285nd@gmail.com>
2025-03-18 17:33:09 +0700

    feat: add source entity

---
+++ src/main/java/com/haiphamcoder/cdp/domain/entity/Source.java
@@ -0,0 +1,107 @@
+package com.haiphamcoder.cdp.domain.entity;
+
+import java.sql.Timestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.haiphamcoder.cdp.shared.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
+@Entity
+@Table(name = "source")
+public class Source extends BaseEntity{
+    @Id
+    @Column(name = "id", nullable = false)
+    @JsonProperty("id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    @JsonProperty("name")
+    private String name;
+
+    @Column(name = "description", nullable = true)
+    @JsonProperty("description")
+    private String description;
+
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    @JsonProperty("type")
+    private SourceType type;
+
+    @Column(name = "config", nullable = false)
+    private JsonNode config;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    @JsonProperty("status")
+    private SourceStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonProperty("user")
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "folder_id", nullable = false)
+    @JsonProperty("folder")
+    private Folder folder;
+
+    @Column(name = "is_deleted", nullable = false)
+    @Builder.Default
+    @JsonProperty("is_deleted")
+    private Boolean isDeleted = false;
+
+    @Column(name = "is_starred", nullable = false)
+    @Builder.Default
+    @JsonProperty("is_starred")
+    private Boolean isStarred = false;
+
+    @Column(name = "last_sync_time", nullable = true)
+    @JsonProperty("last_sync_time")
+    private Timestamp lastSyncTime;
+
+    @RequiredArgsConstructor
+    public enum SourceType {
+        FILE("file"),
+        DATABASE("database");
+
+        @Getter
+        private final String value;
+    }
+
+    @RequiredArgsConstructor
+    public enum SourceStatus {
+        ACTIVE("active"),
+        INACTIVE("inactive");
+
+        @Getter
+        private final String value;
+    }
+}
