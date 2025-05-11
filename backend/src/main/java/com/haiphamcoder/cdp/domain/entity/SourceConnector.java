28c5b4f2deb37a8ed2631f8d06408640a0393fcf
Hai Pham Ngoc <ngochai285nd@gmail.com>
2025-05-11 23:17:22 +0700

    feat: update

--- backend/src/main/java/com/haiphamcoder/cdp/domain/entity/SourceConnector.java
+++ backend/src/main/java/com/haiphamcoder/cdp/domain/entity/SourceConnector.java
@@ -1,24 +1,15 @@
 package com.haiphamcoder.cdp.domain.entity;
 
-import java.sql.Timestamp;
-import java.util.List;
-import java.util.Map;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.haiphamcoder.cdp.shared.BaseEntity;
-import com.haiphamcoder.cdp.shared.converter.MapStringConverter;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -54,6 +45,11 @@ public class SourceConnector extends BaseEntity {
     @JsonProperty("description")
     private String description;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "source_id")
+    @JsonProperty("source")
+    private Source source;
+
     
-    
+
 }
