c9b037f0f1ae0f53c9a6de25c181abadd0abacd8
Hai Pham Ngoc <ngochai285nd@gmail.com>
2025-05-19 22:59:59 +0700

    feat: update user-management-service

--- user-management-service/src/main/java/com/haiphamcoder/usermanagement/controller/UserController.java
+++ user-management-service/src/main/java/com/haiphamcoder/usermanagement/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.haiphamcoder.usermanagement.domain.dto.UserDto;
@@ -18,6 +19,7 @@ import com.haiphamcoder.usermanagement.shared.http.RestAPIResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping(path = "/")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
