7927b04b8be0ef874e1529ce60b9c36723da8879
Hai Pham Ngoc <ngochai285nd@gmail.com>
2025-06-26 16:01:21 +0700

    env: split to local and production environment

--- database/mysql-server/init/init-database.sh
+++ database/mysql-server/init/init-database.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-mysql -u root -p root sandbox < "/docker-entrypoint-initdb.d/001-create-database.sql"
-mysql -u root -p root sandbox < "/docker-entrypoint-initdb.d/002-create-tables.sql"
-mysql -u root -p root sandbox < "/docker-entrypoint-initdb.d/003-create-source-table.sql"
-mysql -u root -p root sandbox < "/docker-entrypoint-initdb.d/004-create-connector.sql"
-mysql -u root -p root sandbox < "/docker-entrypoint-initdb.d/005-create-chart-table.sql"
-mysql -u root -p root sandbox < "/docker-entrypoint-initdb.d/006-create-report-table.sql"
+echo "Creating reporting user..."
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED WITH caching_sha2_password BY '${MYSQL_PASSWORD}';" 2>/dev/null
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%';" 2>/dev/null
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;" 2>/dev/null
+
+echo "Creating database..."
+mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < "/docker-entrypoint-initdb.d/001-create-database.sql" 2>/dev/null
+mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < "/docker-entrypoint-initdb.d/002-create-tables.sql" 2>/dev/null
+mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < "/docker-entrypoint-initdb.d/003-create-source-table.sql" 2>/dev/null
+mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < "/docker-entrypoint-initdb.d/004-create-connector.sql" 2>/dev/null
+mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < "/docker-entrypoint-initdb.d/005-create-chart-table.sql" 2>/dev/null
+mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < "/docker-entrypoint-initdb.d/006-create-report-table.sql" 2>/dev/null
