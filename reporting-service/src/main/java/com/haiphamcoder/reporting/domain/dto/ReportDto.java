c4fa2411d1d6dcbeeb27f981a35fe33535ce970f
Hai Pham Ngoc <ngochai285nd@gmail.com>
2025-07-08 16:23:18 +0700

    chore: remove unused import

--- reporting-service/src/main/java/com/haiphamcoder/reporting/domain/dto/ReportDto.java
+++ reporting-service/src/main/java/com/haiphamcoder/reporting/domain/dto/ReportDto.java
@@ -8,8 +8,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.haiphamcoder.reporting.domain.enums.ReportPermissionType;
 
